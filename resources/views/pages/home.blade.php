0d4c0879cc133074e0c10fb70429ee6df2aec163
codingcele <marcocelegato9@gmail.com>
2023-02-12 23:17:15 +0100

    home con lista people

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1>
-        PEOPLE
-    </h1>
+    <h1>People</h1>
+    <ul>
+        @foreach ($people as $person)
+            <li>
+                {{ $person -> firstName }} {{ $person -> lastName }} - {{ $person -> dateOfBirth }}
+            </li>
+        @endforeach
+    </ul>
 @endsection
